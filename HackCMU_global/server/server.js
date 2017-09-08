e57b3fa845515465861ad6c375c68ed28c3e4339
RuohaiGe <geruohai97@outlook.com>
2017-09-08 19:42:02 -0400

    new Task init

---
+++ HackCMU_global/server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+// connect to the mongodb local or on heroku
+// URI is provided by heroku in the .env file
+
+mongoose.Promise = global.Promise;
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Lunch-vote',{
+//   useMongoClient: true,
+// });
+
+mongoose.connect('mongodb://localhost:27017/Lunch-vote',{
+  useMongoClient: true,
+});
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+//Prepare for the ejs path and template
+app.set('views', __dirname + '/../client/view');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+        
+//make the '/' path relate to local client folder
+app.use('/', express.static(path.join(__dirname, '/../client')));
+
+app.get('/',function (req,res){
+    res.render('index')
+})
+
+app.listen(port,process.env.Listen_URL);
