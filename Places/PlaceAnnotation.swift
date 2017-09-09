a8bcc8985c022a3f55453f7f500d6e9c4c32841a
Zheng Xu <zhengx@weh5201-b1.andrew.cmu.edu>
2017-09-08 21:54:24 -0400

    Initiated a demo project

---
+++ Places/PlaceAnnotation.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceAnnotation.swift
+//  Places
+//
+//  Created by Zheng Xu on 9/8/17.
+//  Copyright © 2017 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class PlaceAnnotation: NSObject, MKAnnotation {
+  let coordinate: CLLocationCoordinate2D
+  let title: String?
+  
+  init(location: CLLocationCoordinate2D, title: String) {
+    self.coordinate = location
+    self.title = title
+    
+    super.init()
+  }
+}
+
