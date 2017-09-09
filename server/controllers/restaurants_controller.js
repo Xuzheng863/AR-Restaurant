b8627c13a9507384fa071321c55b10e336417f78
RuohaiGe <geruohai97@outlook.com>
2017-09-08 22:03:31 -0400

    Database Fetch 2017.09.08 22:02
    Fill the database

--- server/controllers/restaurants_controller.js
+++ server/controllers/restaurants_controller.js
@@ -1,125 +1,32 @@
-// const Restaurant = require('../db/Restaurant.js')
-// const Vote= require('../db/Vote.js');
-// const request = require('request')
-// const APIKEY_geocode = 'AIzaSyAI9YglLGrKtH8kTHaIVb1H_c_h206eOv8';
-// const APIKEY_search = 'AIzaSyC_-bl7-_WVyoBBlhiyUXBi_Dbxex6NWPM';
+const Restaurant = require('../db/Restaurant.js')
+const Vote= require('../db/Vote.js');
+const request = require('request')
+const APIKEY_search = 'AIzaSyDgXVhtYw1RwGQSajpSlIwufcsPHciE4U0';
 
-// module.exports = {
+module.exports = {
+    create_google(req,res){
+        var lat = 40.4430055;
+        var lng = -79.94606759999999;
+        var url_detail = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1000&type=restaurant&key=${APIKEY_search}`
+        request.get(url_detail,function(error,response,body){
+            console.log('error:',error);
+            console.log('statusCode:',response && response.statusCode);
+            var body = JSON.parse(body);
+            var index = 0;
+            var new_restaurant = Restaurant({
+                name: body.results[index].name,
+                address: body.results[index].vicinity,
+                geoposition : [body.results[index].geometry.location.lat,body.results[index].geometry.location.lng],
+                price_level : body.results[index].price_level,
+                comment : "Good!"
+            });
 
-//     index(req,res){
-//         //send all the restaurants in the database to the 
-//         //client side as an array with json object as elements
-//        Restaurant.find({},function(err,restaurants){
-//                 if(err){
-//                     console.log(err);
-//                 }else{
-//                     res.json(restaurants);
-//                 }
-//        });
-//     },
+            //save the new restaurant to Mongodb
+            new_restaurant.save((err) => {
+                if (err) throw err;
 
-//     create_google(req,res){
-//         var restaurant = req.body;
-//         var lat = restaurant.lat;
-//         var lng = restaurant.lng;
-//         var url_geocode = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${APIKEY_geocode}`
-//         request.get(url_geocode,function(error,response,body){
-//             console.log('error:',error);
-//             console.log('statusCode:',response && response.statusCode);
-//             var body = JSON.parse(body);
-//             var placeId = body.results[0].place_id
-//             var url_detail = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=1&type=restaurant&key=${APIKEY_search}`
-//             request.get(url_detail,function(error,response,body){
-//                     console.log('error:',error);
-//                     console.log('statusCode:',response && response.statusCode);
-//                     var body = JSON.parse(body);
-//                     var new_restaurant = Restaurant({
-//                         name: body.results[0].name,
-//                         address: body.results[0].vicinity
-//                     });
-        
-//                     //save the new restaurant to Mongodb
-//                     new_restaurant.save((err) => {
-//                         if (err) throw err;
-        
-//                         console.log('restaurant saved!');
-//                     });
-//             })
-//         })
-//         res.sendStatus(200);
-//     },
-        
-//     // ES6 version of create : function(req,res)
-//     create(req,res){
-//         //get the information from the website
-//         var restaurant = req.body;
-//         var new_restaurant = Restaurant({
-//             name: restaurant.name,
-// 	        address: restaurant.address,
-//         });
-
-//         //save the new restaurant to Mongodb
-//         new_restaurant.save((err) => {
-//             if (err) throw err;
-
-//             console.log('restaurant saved!');
-//         });
-        
-//         res.sendStatus(200);
-
-//     },
-
-//     delete(req,res){
-//         var restaurant_id = req.params.id;
-//         //find the id from mongoDB and also remove it 
-//         Restaurant.findByIdAndRemove({_id : restaurant_id},function(err,restaurant){
-//                 if(err){
-//                     console.log(err);
-//                 }else{
-//                     res.json(restaurant);
-//                 }
-//         });
-//         Vote.remove({restaurant_id : restaurant_id},function(err){
-//                 if(err){
-//                     console.log(err);
-//                 }
-//         });
-//     },
-
-//     view(req,res){
-//         var restaurant_id = req.params.id;
-//         Restaurant.findById({_id : restaurant_id},function(err,restaurant){
-//                 if(err){
-//                     console.log(err);
-//                 }else{
-//                     res.json(restaurant);
-//                 }
-//         });
-//     },
-
-//     edit(req,res){
-//         var restaurant_id = req.params.id;
-//         const restaurantProps = req.body;
-
-//         Restaurant.update({ _id: restaurant_id }, restaurantProps, 
-//                 function(err,restaurant){
-//                     if(err){
-//                         console.log(err);
-//                     }else{
-//                         console.log("Update Successfully");
-//                     }
-//                 })
-//         //change all the vote related to this restaurant
-//         Vote.find({"restaurant_id" : restaurant_id }).exec()
-//           .then((doc) =>
-//             {
-//               for(var index = 0;index < doc.length;index ++){
-//                 doc[index].restaurant_name = restaurantProps.name;
-//                 doc[index].save();
-//               }
-//             }
-//         );
-
-//         res.sendStatus(200);
-//     }
-// }
+                console.log('restaurant saved!');
+            });
+        })
+    }
+}
--- server/db/Restaurant.js
+++ server/db/Restaurant.js
@@ -1,24 +1,42 @@
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-// // build a new schema for the restaurant type
-// var RestaurantSchema = new Schema({
-// 	name: {
-// 		type : String,
-// 		//Set index for the schema to increase the search speed
-// 		index : 1,
-// 		//Set Uniqueness to ensure that nothing conflict
-// 		unique : true,
-// 		required : true
-// 	},
-// 	address: {
-// 		type : String,
-// 		index : 1,
-// 		unique : true,
-// 		required : true
-// 	}
-// });
+// build a new schema for the restaurant type
+var RestaurantSchema = new Schema({
+	name: {
+		type : String,
+		//Set index for the schema to increase the search speed
+		index : 1,
+		//Set Uniqueness to ensure that nothing conflict
+		unique : true,
+		required : true
+	},
+	address: {
+		type : String,
+		index : 1,
+		unique : true,
+		required : true
+    },
+    geoposition : {
+        type : [int, int],
+        index : 1,
+		unique : true,
+		required : true
+    },
+    comment : {
+        type : string,
+        index : 1,
+		unique : true,
+		required : true
+    },
+    price_level : {
+        type : int,
+        index : 1,
+		unique : true,
+		required : true
+    }
+});
 
-// var Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+var Restaurant = mongoose.model('Restaurant', RestaurantSchema);
 
-// module.exports = Restaurant;
+module.exports = Restaurant;
